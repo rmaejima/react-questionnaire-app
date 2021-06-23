5465b2c35b0732bf360107c74e4d231a9993283d
rmaejima <ryo.maejii@gmail.com>
2021-06-24 00:33:54 +0900

    Fix api

---
+++ src/api/videos.ts
@@ -0,0 +1,100 @@
+import axios from "axios";
+import { YoutubeVideo } from "types/youtube";
+
+const YOUTUBE_SERACH_API_URI = "https://www.googleapis.com/youtube/v3/search?";
+const API_KEY = `${process.env.REACT_APP_YOUTUBE_API_KEY}`;
+
+export const getYoutubeVideos = async (
+  keyword: string
+): Promise<YoutubeVideo[]> => {
+  const params = {
+    key: API_KEY,
+    q: `${keyword}`,
+    type: "video",
+    maxResults: "5",
+    order: "viewCount", // 検索順
+  };
+  const queryParams = new URLSearchParams(params);
+
+  try {
+    const res = await axios.get(YOUTUBE_SERACH_API_URI + queryParams);
+    const result = await JSON.parse(JSON.stringify(res.data));
+    const items: YoutubeVideo[] = await result.items;
+    return items;
+  } catch (err) {
+    // alert(err);
+    return err;
+  }
+};
+
+//
+//
+//
+//
+//tsxで書いていた時の記録
+
+// interface Props {
+//   keyword?: string;
+// }
+
+// export const YoutubeItem: React.VFC<Props> = ({ keyword }) => {
+//   const [videos, setVideos] = useState<YoutubeVideo[]>([]);
+
+//   useEffect(() => {
+//     // クエリ文字列を定義する
+//     const params = {
+//       key: API_KEY,
+//       q: `${keyword}`, // 検索キーワード
+//       type: "video",
+//       maxResults: "10", // 結果の最大数
+//       order: "viewCount", // 結果の並び順を再生回数の多い順に
+//     };
+//     const queryParams = new URLSearchParams(params);
+
+//     // APIをコールする
+//     (async () => {
+//       try {
+//         const res = await axios.get(YOUTUBE_SERACH_API_URI + queryParams);
+//         const result = await JSON.parse(JSON.stringify(res.data));
+//         const items = await result.items;
+//         console.log(items);
+//         setVideos(items);
+//       } catch (err) {
+//         alert(err);
+//       }
+//     })();
+//   }, []);
+
+//   return (
+//     <Container>
+//       {videos[0] &&
+//         videos.map((video) => {
+//           return (
+//             <Iframe
+//               title="サンプル"
+//               id="player"
+//               width="640"
+//               height="360"
+//               src={"https://www.youtube.com/embed/" + video.id.videoId}
+//               frameBorder="0"
+//               allowFullScreen
+//             />
+//           );
+//         })}
+//     </Container>
+//   );
+// };
+
+// const Container = styled.div`
+//   display: grid;
+//   grid-template-columns: repeat(auto-fit, minmax(640px, 1fr));
+//   gap: 1rem;
+//   /* max-width: 1480px; */
+//   margin: 0 auto;
+//   padding: 0 2rem;
+// `;
+
+// const Iframe = styled.iframe`
+//   display: block;
+//   margin: 0 auto;
+// `;
--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 import { useScoreContext } from "contexts/ScoreProvider";
 import { PageHeading } from "components/common/PageHeading";
 import { Button } from "components/common/Button";
-import { YoutubeItem } from "api/YoutubeItem";
+import { getYoutubeVideos } from "api/videos";
+import { YoutubeVideo } from "types/youtube";
 
 export const Result: React.VFC = () => {
   const { score, setScore } = useScoreContext();
+  const [videos, setVideos] = useState<YoutubeVideo[]>([]);
+  useEffect(() => {
+    (async () => {
+      setVideos(await getYoutubeVideos(score));
+    })();
+  }, []);
   return (
     <Container>
       <PageHeading>結果</PageHeading>
       <Text>こんな動画はどうでしょうか？</Text>
-      <YoutubeItem keyword={score} />
-
+      {videos[0] &&
+        videos.map((video) => {
+          return (
+            <Iframe
+              title="サンプル"
+              id="player"
+              width="640"
+              height="360"
+              src={"https://www.youtube.com/embed/" + video.id.videoId}
+              frameBorder="0"
+              allowFullScreen
+            />
+          );
+        })}
       <Link to="/">
         <StyledButton>
           <Button>ホームに戻る</Button>
@@ -40,4 +59,9 @@ const Text = styled.p`
 const StyledButton = styled.div`
   max-width: 1200px;
   margin: 2rem auto 0;
+`;
+
+const Iframe = styled.iframe`
+  display: block;
+  margin: 0 auto;
 `;
